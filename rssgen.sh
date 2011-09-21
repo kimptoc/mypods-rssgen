ec606597d0aedfc5eb9378a8be071a8fa325492b
Chris Kimpton <chris@kimptoc.net>
2011-09-21 18:03:28 +0100

    chkpt

---
+++ rssgen.sh
@@ -0,0 +1,43 @@
+
+function writeRssPart()
+{
+  MP3FILE=$1
+
+  MP3RSS=${MP3FILE}.rss
+
+  if [ ! -f $MP3FILE ]; then
+    # no mp3 file - so give up
+    return
+  fi
+
+  if [ -f $MP3RSS ]; then
+    # rss file is there already, so do nothing
+    return
+  fi
+
+  PUBDATE=`date +"%d %b %C%y %H:%M:%S GMT"`
+
+  echo "Processing file $MP3FILE"
+    echo "<item><title>$MP3FILE</title>" >>$MP3RSS
+  echo "<pubDate>$PUBDATE</pubDate>" >>$MP3RSS
+  echo "<guid isPermaLink='false'>$MP3FILE</guid>" >>$MP3RSS
+  echo '<enclosure url="http://home.kimptoc.net:8080/radioshifted/mypods/'$MP3FILE'" type="audio/mpeg" length="0" />' >>$MP3RSS
+  echo "</item>" >> $MP3RSS
+}
+
+cd ../mypods/
+
+for FILE in *.mp3; do
+  writeRssPart $FILE
+done
+
+#Assemble parts into complete RSS feed
+cat ../scripts/my.rss.header > my.rss
+
+for PART in *dio4*.rss; do
+  cat $PART >> my.rss
+done
+
+cat ../scripts/my.rss.footer >> my.rss
+
+
