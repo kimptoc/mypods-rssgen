78faf70d2ae1925601326e6fa3df926e3b78f569
Chris Kimpton <chris@kimptoc.net>
2017-02-04 15:59:05 +0000

    chkpt - latest

--- rssgen.sh
+++ rssgen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /home/kimptoc/public_html/radioshifted/scripts
+export SCRIPT_DIR=/Users/family/Sites/box/hijacked/scripts/
+
+cd $SCRIPT_DIR
 
 function writeRssPart()
 {
@@ -24,33 +26,36 @@ function writeRssPart()
     echo "<item><title>$MP3FILE</title>" >>$MP3RSS
   echo "<pubDate>$PUBDATE</pubDate>" >>$MP3RSS
   echo "<guid isPermaLink='false'>$MP3FILE</guid>" >>$MP3RSS
-  echo '<enclosure url="http://home.kimptoc.net:8080/radioshifted/mypods/'$MP3FILE'" type="audio/mpeg" length="0" />' >>$MP3RSS
+  echo '<enclosure url="http://home.kimptoc.net:8080/hijacked/podcast/'$MP3FILE'" type="audio/mpeg" length="0" />' >>$MP3RSS
   echo "</item>" >> $MP3RSS
 }
 
-cd ../mypods/
+cd ../podcast
 
-find . -ctime +30 -exec mv {} ../archives \;
+find . -ctime +30 -exec mv {} archived \;
 
 for FILE in *.mp3; do
   writeRssPart $FILE
 done
 
 #Assemble parts into complete RSS feed
-cat ../scripts/my.rss.header > my.rss
+
+# Radio 4 rss
+
+cat $SCRIPT_DIR/my.rss.header > my.rss
 
 for PART in *dio4*.rss; do
   cat $PART >> my.rss
 done
 
-cat ../scripts/my.rss.footer >> my.rss
+cat $SCRIPT_DIR/my.rss.footer >> my.rss
 
-cat ../scripts/my.rss.header2 > my2.rss
+# Radio 2 rss
+
+cat $SCRIPT_DIR/my.rss.header2 > my2.rss
 
 for PART in *dio2*.rss; do
   cat $PART >> my2.rss
 done
 
-cat ../scripts/my.rss.footer >> my2.rss
-
-
+cat $SCRIPT_DIR/my.rss.footer >> my2.rss
